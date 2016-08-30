ece5b9f6e6f7987b4af4155343da7a20673c739f
Hugal31 <hugo.laloge@epitech.eu>
2016-08-30 18:39:20 +0200

    refactor: Use pflag for posix flag parsing
    - Use github.com/ogier/pflag package instead of flag
    - Put the flag parsing into the package command

--- main.go
+++ main.go
@@ -1,47 +1,9 @@
 package main
 
 import (
-	"flag"
-	"fmt"
-	"os"
-
 	"github.com/Hugal31/mePicture/command"
 )
 
-var help bool
-
-func usage() {
-	fmt.Fprintln(os.Stderr, "mePicture is a tool for managing pictures with tag and settup wallpapers slideshows\n"+
-		"\n"+
-		"Usage:\n"+
-		"\n"+
-		"\tmePicture command [arguments]\n"+
-		"\n"+
-		"The commands are:\n"+
-		"\n"+
-		"\tpicture     List, add and delete pictures\n"+
-		"\ttag         List, add and delete tags")
-	os.Exit(1)
-}
-
-func init() {
-	flag.BoolVar(&help, "h", false, "Help")
-	flag.Parse()
-}
-
 func main() {
-	if help || flag.NArg() == 0 {
-		usage()
-	}
-
-	switch flag.Arg(0) {
-	case "picture":
-		command.CommandPicture(flag.Args()[1:])
-		break
-	case "tag":
-		command.CommandTag(flag.Args()[1:])
-		break
-	default:
-		usage()
-	}
+	command.Run()
 }
